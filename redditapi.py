4552c792b96605699f5587b8117da78dbd2ec758
Kel Zhang <awesomekel@Qiwens-MacBook-Pro-2.local>
2017-06-01 16:28:50 +0800

    trying to deploy my first flask app on heroku

--- redditapi.py
+++ redditapi.py
@@ -47,4 +47,6 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
  
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.debug = True
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host='0.0.0.0', port=port)
