f3a4f78e43c3e413c02f5ccd27885aa23aa7490b
Kel Zhang <awesomekel@Qiwens-MacBook-Pro-2.local>
2017-06-01 17:07:05 +0800

    still trying to fix

--- redditapi.py
+++ redditapi.py
@@ -2,7 +2,6 @@
 import requests
 import json
 import bs4
-import os
 from flask import Flask, jsonify
 from flask import make_response
  
@@ -48,6 +47,4 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    app.debug = True
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
